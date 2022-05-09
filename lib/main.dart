3589cdd33d317a89e480bc5d89a22296c24c1d55
Sachin Shah <sachinshah1999@gmail.com>
2022-05-10 03:03:51 +0530

    day 13 completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:catalog_app/pages/login_page.dart';
 import 'package:catalog_app/utils/routes.dart';
 import 'package:catalog_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,36 @@
 import 'package:catalog_app/models/catalog.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
-  // HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  // HomePage({Key? key}) : super(key: key);
   String name = "imsachinshah";
+
   int days = 30;
+
   final dummyItems = List.generate(10, (index) => Catalog.items[0]);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson = await rootBundle.loadString('assets/files/catalog.json');
+    final decodeData = jsonDecode(catalogJson);
+    var productData = decodeData["products"];
+    print(productData);
+  }
 
   @override
   Widget build(BuildContext context) {
