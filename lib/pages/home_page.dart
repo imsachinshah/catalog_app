981d66b8498c176630eed8ec1aff7c3e1352883d
Sachin Shah <sachinshah1999@gmail.com>
2022-05-11 03:17:46 +0530

    day 15 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,8 +16,6 @@ class _HomePageState extends State<HomePage> {
 
   int days = 30;
 
-  final dummyItems = List.generate(10, (index) => Catalog.items[0]);
-
   @override
   void initState() {
     // TODO: implement initState
@@ -26,10 +24,14 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
-    final catalogJson = await rootBundle.loadString('assets/files/catalog.json');
+    await Future.delayed(const Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString('assets/files/catalog.json');
     final decodeData = jsonDecode(catalogJson);
     var productData = decodeData["products"];
-    print(productData);
+    Catalog.items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
   }
 
   @override
@@ -43,14 +45,18 @@ class _HomePageState extends State<HomePage> {
       )),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyItems.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: dummyItems[index],
-            );
-          },
-        ),
+        child: (Catalog.items != null )
+            ? ListView.builder(
+                itemCount: Catalog.items!.length,
+                itemBuilder: (context, index) {
+                  return ItemWidget(
+                    item: Catalog.items![index],
+                  );
+                },
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
